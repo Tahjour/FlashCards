acd5289aab15b5c3d2fe1da5107820fcd8bf33ed
Tahjour <tahjallman64@gmail.com>
2021-02-25 18:07:49 -0500

    Finished Required Tasks

--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.flashcardapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView questionTextView = findViewById(R.id.question);
+        TextView answerTextView = findViewById(R.id.answer);
+        answerTextView.setVisibility(View.INVISIBLE);
+
+        questionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                questionTextView.setVisibility(View.INVISIBLE);
+                answerTextView.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
 }
